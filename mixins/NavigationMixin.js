7a67e4f8dd3eb6a2ad3939edeebe05a31a6141cf
Iqbal 79 <iqbalvandame2134@gmail.com>
2021-04-30 22:22:54 +0700

    fixing checkout

--- mixins/NavigationMixin.js
+++ mixins/NavigationMixin.js
@@ -1,7 +1,26 @@
 export const NavigationMixin = {
+  data(){
+    return {
+      currentRoute:'/',
+      currIdProduct:null,
+    };
+  },
     methods: {
-        goToPage(route) {
-          this.$emit('update-route', route);
-        }
+      goToPage(route) {
+        this.$emit('update-route', route);
+      },
+      updateCart(id){
+        console.log(id);
+        this.$emit('update-id-product', id);
+        this.goToPage('/buy');
+      },
+      updateIdProduct(id){
+        this.currIdProduct = id;
+        this.updateCart(id);
+      },
+      updateCurrRoute(route){
+        this.currRoute = route;
+        this.goToPage(route);
+      }
     },
 }
--- pages/PaymentPage.js
+++ pages/PaymentPage.js
@@ -1,9 +1,36 @@
 import { FormatCurrencyMixin } from "../mixins/FormatCurrencyMixin.js";
 import { NavigationMixin } from "../mixins/NavigationMixin.js";
+import { FetchDataMixin } from "../mixins/FetchDataMixin.js";
 
 export const PaymentPage = {
-    mixins: [FormatCurrencyMixin, NavigationMixin],
-    props: ["purchasedProduct"],
+    mixins: [FormatCurrencyMixin, NavigationMixin,FetchDataMixin,],
+    props: ['idproduct'],
+    data(){
+        return{
+            purchasedProduct:{
+                name:"",
+                qty:1,
+                image:"",
+                price:0
+            },
+            totalBayar:0,
+    };
+    },
+    created(){
+        console.log(this.products)
+        this.readTextFile("../data/allproducts.json", (text) => {
+            var products = JSON.parse(text);
+            var _product = products[this.idproduct-1];
+            console.log(_product);
+
+            this.purchasedProduct = {
+                name : _product.name,
+                price : _product.price,
+                qty:1,
+                image : _product.image
+            }
+        });
+    },
     methods: {
         decreaseQty(){
             this.purchasedProduct.qty = this.purchasedProduct.qty-1;
@@ -13,20 +40,31 @@ export const PaymentPage = {
         },
         calculatePrice(){
             this.totalBayar = this.purchasedProduct.qty * this.purchasedProduct.price;
-        }
+        },
+        readTextFile(file, callback) {
+            var rawFile = new XMLHttpRequest();
+            rawFile.overrideMimeType("application/json");
+            rawFile.open("GET", file, true);
+            rawFile.onreadystatechange = function() {
+                if (rawFile.readyState === 4 && rawFile.status == "200") {
+                    callback(rawFile.responseText);
+                }
+            }
+            rawFile.send(null);
+        },
     },
     template: `
     <section class="position-relative mt-100 py-5">
         <div class="wrapper">
             <div class="row mb-5">
                 <div class="col-lg-12">
-                    <h3 class="font-weight-bold">Cart</h3>
+                    <h3 class="font-weight-bold">Checkout</h3>
                 </div>
             </div>
             <div class="bg-white p-5">
                 <div class="row">
                     <div class="col-3">
-                        <img src="assets/images/asus_tuf.jpeg" style="width: 100%;"/>
+                        <img :src="'assets/images/'+purchasedProduct.image" style="width: 100%;"/>
                     </div>
                     <div class="col-1"></div>
                     <div class="col-8">
@@ -37,7 +75,7 @@ export const PaymentPage = {
                                 </h3>
                                 <h4 class="font-weight-bold">
                                     {{formatCurrency(purchasedProduct.price)}}
-                                </h3>
+                                </h4>
                             </div>
                         </div>
                         <div class="row">
@@ -58,7 +96,7 @@ export const PaymentPage = {
                             </div>
                         </div>
                         <div class="row" v-if="totalBayar !== 0">
-                            <div class="col-3">
+                            <div class="col-4">
                                 <h5 class="font-weight-bold">Total Bayar : </h5> 
                             </div>
                             <div class="col-8">
